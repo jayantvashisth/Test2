d2e6740c6ba35502cc28fc78009bd9f313fb01ea
jayantvashisth <vashisthjayant@gmail.com>
2024-03-22 16:48:23 +0530

    sidebar fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { DiscussionForum } from "@/components/discussionForum";
 import { MarketStories } from "@/components/marketStories";
 import { SideMenu } from "@/components/sideMenu";
@@ -7,30 +7,15 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [showSideBar, setShowSideBar] = useState(true);
+  const [showSideBar, setShowSideBar] = useState(false);
 
   const toggleSideBar = () => {
-    if(showSideBar){
-      setShowSideBar(false)
-    }
-    else{
-      setShowSideBar(true)
-    }
-    // setShowSideBar(!showSideBar); // Toggle the showSideBar state
-    console.log("hello");
+    setShowSideBar(!showSideBar);
   };
-
-  // useEffect(() => {
-  //   console.log("showSideBar updated:", showSideBar);
-  // }, [showSideBar]);
 
   return (
     <main className="flex flex-row w-full">
-      <div
-        onClick={() => {
-          setShowSideBar(false);
-        }}
-      >
+      <div>
         <SideMenu showSideBar={showSideBar} toggleSideBar={toggleSideBar} />
       </div>
       <div className="flex flex-row justify-center w-3/5">
