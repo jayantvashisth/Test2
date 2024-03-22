e44d382399038bad6000956546cdbb329c6ed27f
jayantvashisth <vashisthjayant@gmail.com>
2024-03-22 21:01:14 +0530

    sidebar updated

--- src/components/sideMenu.jsx
+++ src/components/sideMenu.jsx
@@ -1,25 +1,50 @@
 import clsx from "clsx";
 import React from "react";
+import { AiFillCaretUp } from "react-icons/ai";
+import {
+  FaRegCommentDots,
+  FaCaretDown,
+  FaCircleUser,
+  FaBell,
+  FaCommentDollar
+} from "react-icons/fa6";
+import { SideMenuHeader, SideMenuTabs } from "./sideMenuTabs";
 
 export const SideMenu = ({ showSideBar, toggleSideBar }) => {
   return (
     <div
       className={clsx(
-        "flex flex-row w-1/5 bg-blue-600 x-0 left-0 p-10 pl-20 text-white fixed h-full transition-transform duration-300 ease-in-out",
+        "flex flex-row w-1/5 bg-purple-950 x-0 left-0 pt-2 text-white fixed h-full transition-transform duration-300 ease-in-out",
         {
           "transform translate-x-1000": showSideBar,
           "transform -translate-x-full": !showSideBar,
         }
       )}
     >
-      <div className="header">
-        <span>Hello, User</span>
+      <div className="flex flex-col gap-2 w-full items-start justify-start px-2">
+        <SideMenuHeader
+          icon1={<FaCircleUser className="h-[30px] w-[30px]" />}
+          text={"Hello, User"}
+          icon2={<FaBell className="h-[20px] w-[20px]" />}
+        />
+        <SideMenuTabs
+          icon1={<FaRegCommentDots />}
+          text={"Discussion Forum"}
+          icon2={<FaCaretDown />}
+        />
+        <SideMenuTabs icon1={<FaCommentDollar />} text={"Market Stories"} />
+        <SideMenuTabs text={"Sentiment"} />
+        <SideMenuTabs text={"Market"} />
+        <SideMenuTabs text={"Sector"} />
+        <SideMenuTabs text={"Watchlist"} />
+        <SideMenuTabs text={"Events"} />
+        <SideMenuTabs text={"News/Interview"} />
       </div>
       <button
         onClick={toggleSideBar}
-        className="w-[66px] rotate-90 fixed top-1/2 right-[-40px] bg-blue-600"
+        className="w-[66px] rotate-90 fixed flex items-center justify-center top-1/2 right-[-40px] bg-purple-950"
       >
-        Click Me
+        <AiFillCaretUp />
       </button>
     </div>
   );
---
+++ src/components/sideMenuTabs.jsx
@@ -0,0 +1,30 @@
+import clsx from "clsx";
+import React from "react";
+
+export const SideMenuHeader = ({ icon1, text, icon2 }) => {
+  return (
+    <div className="flex flex-row justify-between items-center h-[60px] border-b w-full px-3">
+      <div className="flex flex-row items-center gap-2">
+        {icon1}
+        <span>{text}</span>
+      </div>
+      {icon2}
+    </div>
+  );
+};
+
+export const SideMenuTabs = ({ icon1, text, icon2 }) => {
+  return (
+    <div className="flex flex-row justify-between items-center hover:bg-slate-700 h-[40px] w-full px-3 cursor-pointer">
+      <div
+        className={clsx("flex flex-row items-center gap-2", {
+          "pl-6": !icon1,
+        })}
+      >
+        {icon1}
+        <span>{text}</span>
+      </div>
+      {icon2}
+    </div>
+  );
+};
